31963a448980a31662fe61271575949fa6b591c1
mmamorim <marc.m.amorim@gmail.com>
2025-02-26 10:22:32 -0300

    v1

--- Aula02_26FEV/main.js
+++ Aula02_26FEV/main.js
@@ -1,6 +1,25 @@
 
 console.log("Vamos jogar palitinhos?")
 
-let resp = prompt("Qual sua idade?")
-resp = parseInt(resp)
-console.log(resp+6)
+let nomeJog = prompt("Qual seu nome?")
+let qtdePalitosJog = 3
+let qtdeEscolhaJog = 0
+let qtdeChuteJog = 0
+
+let nomeComp = "Computador"
+let qtdePalitosComp = 3
+let qtdeEscolhaComp = 0
+let qtdeChuteComp = 0
+
+while(qtdePalitosJog > 0 && qtdePalitosComp > 0) {
+
+    qtdeEscolhaJog = prompt("Quantos palitos vc quer separar?")
+    qtdeEscolhaJog = parseInt(qtdeEscolhaJog)
+
+    qtdeEscolhaComp = (parseInt(Math.random()*10) % qtdePalitosComp)+1
+ 
+    console.log("qtdeEscolhaJog",qtdeEscolhaJog);
+    console.log("qtdeEscolhaComp",qtdeEscolhaComp);
+    break
+
+}
