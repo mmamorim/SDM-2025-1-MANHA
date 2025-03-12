50d468226bbfc83f1b0296a7855e98da87c05219
mmamorim <marc.m.amorim@gmail.com>
2025-03-12 10:07:07 -0300

    v1

---
+++ Aula03_12Mar/main.js
@@ -0,0 +1,9 @@
+import { PI, calculaAreaCirculo, calculaCompCirculo } from "./circulos.js"
+
+console.log("Alo mundo!")
+
+console.log("O valor do PI é: "+PI)
+let area = calculaAreaCirculo(5)
+console.log("A área é: "+area)
+let comp = calculaCompCirculo(5)
+console.log("O comprimento é: "+comp)
