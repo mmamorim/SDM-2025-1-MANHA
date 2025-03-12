1902789e7a6fdb4e890b9c997bcb2604db809133
mmamorim <marc.m.amorim@gmail.com>
2025-03-12 10:38:32 -0300

    v1

--- Aula03_12Mar/main.js
+++ Aula03_12Mar/main.js
@@ -1,4 +1,5 @@
 import { PI, calculaAreaCirculo, calculaCompCirculo } from "./circulos.js"
+import { newUser } from "./users.js"
 
 console.log("Alo mundo!")
 
@@ -7,3 +8,9 @@ let area = calculaAreaCirculo(5)
 console.log("A área é: "+area)
 let comp = calculaCompCirculo(5)
 console.log("O comprimento é: "+comp)
+
+let user = newUser("Ana","Alface")
+let user2 = newUser("Bia","Beterraba")
+
+console.log("a variável user contém: ",user);
+console.log("a variável user2 contém: ",user2);
