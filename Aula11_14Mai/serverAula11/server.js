789058bbad98e40e14b525a587e07f5a266f7a26
mmamorim <marc.m.amorim@gmail.com>
2025-05-14 11:50:42 -0300

    v1

--- Aula11_14Mai/serverAula11/server.js
+++ Aula11_14Mai/serverAula11/server.js
@@ -1,43 +1,13 @@
 import { server, db, PORT } from "./initServer.js"
+import rotasFrutas from "./rotasFrutas.js";
+import rotasUsuarios from "./rotasUsuarios.js";
 
 server.get('/', (req, res) => {
     res.send('🙋‍♂️ Hello...route /');
 });
 
-server.get('/frutas', (req, res) => {
-    let frutas = db.get("/frutas")
-    res.status(200).json(frutas)
-});
-
-server.post('/frutas', (req, res) => {
-    let id = db.newID("FRUTA-")
-    let data = { id, ...req.body }
-    db.set("/frutas/"+data.id, data)
-    res.status(201).json({ msg: "Inserção ok.", data })
-});
-
-server.put('/frutas/:id', (req, res) => {
-    let id = req.params.id;
-    let fruta = db.get("/frutas/"+id);
-    if(fruta == null) {
-        res.status(400).json({ msg: "Fruta não existe." })
-        return
-    }
-    let data = { id, ...req.body }
-    db.set("/frutas/"+data.id, data)
-    res.status(201).json({ msg: "Alteração ok.", data })
-});
-
-server.delete('/frutas/:id', (req, res) => {
-    let id = req.params.id;
-    let fruta = db.get("/frutas/"+id);
-    if(fruta == null) {
-        res.status(400).json({ msg: "Fruta não existe." })
-        return
-    }
-    db.set("/frutas/"+id, null)
-    res.status(201).json({ msg: "Exclusão ok." })
-});
+rotasFrutas(server, db)
+rotasUsuarios(server, db)
 
 server.listen(PORT, () => {
     console.log('Server is running on port '+PORT);
